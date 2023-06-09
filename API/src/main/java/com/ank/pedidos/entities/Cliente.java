2725ef3579fce3ed87c41b3bcdfcfe33c3f9beb7
Rafael Fernandes <dvr.rafa@gmail.com>
2023-06-09 17:05:54 -0300

    Insert Cliente

--- API/src/main/java/com/ank/pedidos/entities/Cliente.java
+++ API/src/main/java/com/ank/pedidos/entities/Cliente.java
@@ -1,16 +1,16 @@
 package com.ank.pedidos.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "cliente")
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+    @Column(unique = true)
     private String cpf;
     private String endereco;
 
