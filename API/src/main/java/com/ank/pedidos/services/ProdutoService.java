64537cc906e708a53976485c544ba06d7f2759a5
Rafael Fernandes <dvr.rafa@gmail.com>
2023-07-04 19:27:42 -0300

    crud produtos

--- API/src/main/java/com/ank/pedidos/services/ProdutoService.java
+++ API/src/main/java/com/ank/pedidos/services/ProdutoService.java
@@ -1,6 +1,8 @@
 package com.ank.pedidos.services;
 
 import com.ank.pedidos.entities.Produto;
+import com.ank.pedidos.entities.Produto;
+import com.ank.pedidos.repositories.CategoriaRepository;
 import com.ank.pedidos.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,31 @@ public class ProdutoService {
     @Autowired
     ProdutoRepository produtoRepository;
 
+    @Autowired
+    CategoriaRepository categoriaRepository;
+        
+
+    public Produto save(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
     public List<Produto> findAll(){
         return produtoRepository.findAll();
+    }
+
+    public void delete(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+    public Produto update(Produto produto, Long id){
+        Produto produtoToUpdate = produtoRepository.findById(id).orElseThrow();
+        produtoToUpdate.setNome(produto.getNome());
+        produtoToUpdate.setCategoria(categoriaRepository.findById(produto.getCategoria().getId()).orElseThrow());
+        produtoToUpdate.setValorPadrao(produto.getValorPadrao());
+        return produtoRepository.save(produtoToUpdate);
+    }
+
+    public Produto findById(Long id) {
+        return produtoRepository.findById(id).orElseThrow();
     }
 }
